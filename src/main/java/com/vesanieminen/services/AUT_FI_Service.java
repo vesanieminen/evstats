75a3a6ff92001626818680adb3227f8ab6b98c23
Vesa Nieminen <vesa@vaadin.com>
2023-05-26 20:41:16 +0300

    Fixed the file loading for a jar driven application.

--- src/main/java/com/vesanieminen/services/AUT_FI_Service.java
+++ src/main/java/com/vesanieminen/services/AUT_FI_Service.java
@@ -5,10 +5,11 @@ import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -19,6 +20,7 @@ import java.nio.channels.Channels;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static com.vesanieminen.Utils.getInt;
@@ -54,40 +56,42 @@ public class AUT_FI_Service {
     }
 
     public static Optional<List<EVStats>> loadDataFromFile() throws IOException, URISyntaxException {
-        URL url = AUT_FI_Service.class.getClassLoader().getResource(DEFAULT_CSV_FILENAME);
-        if (url == null) {
-            return Optional.empty();
-        }
-        final var file = new File(url.toURI());
-        final var fileReader = new FileReader(file);
+        try (InputStream inputStream = AUT_FI_Service.class.getClassLoader().getResourceAsStream(DEFAULT_CSV_FILENAME);
+             InputStreamReader inputStreamReader = new InputStreamReader(Objects.requireNonNull(inputStream));
+             BufferedReader reader = new BufferedReader(inputStreamReader)) {
 
-        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
-        CSVReader csvReader = new CSVReaderBuilder(fileReader)
-                .withSkipLines(0)
-                .withCSVParser(parser)
-                .build();
+            CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+            CSVReader csvReader = new CSVReaderBuilder(reader)
+                    .withSkipLines(0)
+                    .withCSVParser(parser)
+                    .build();
 
-        String[] header = csvReader.readNext();
+            String[] header = csvReader.readNext();
 
-        String[] line;
-        final var evStats = new ArrayList<EVStats>();
-        String year = "";
-        while ((line = csvReader.readNext()) != null) {
-            String month = line[0];
-            if (line[0].contains("/")) {
-                final var split = line[0].split("/");
-                year = split[0];
-                month = split[1];
+            String[] line;
+            final List<EVStats> evStats = new ArrayList<>();
+            String year = "";
+            while ((line = csvReader.readNext()) != null) {
+                String month = line[0];
+                if (line[0].contains("/")) {
+                    final var split = line[0].split("/");
+                    year = split[0];
+                    month = split[1];
+                }
+                var date = LocalDate.of(Integer.parseInt(year), getMonth(month), 1);
+                date = date.withDayOfMonth(date.lengthOfMonth());
+                final var evAmount = getInt(line[3]);
+                final var totalAmount = getInt(line[11]);
+                final var otherAmount = totalAmount - evAmount;
+                evStats.add(new EVStats(line[0], date, evAmount, otherAmount, totalAmount));
             }
-            var date = LocalDate.of(Integer.parseInt(year), getMonth(month), 1);
-            date = date.withDayOfMonth(date.lengthOfMonth());
-            final var evAmount = getInt(line[3]);
-            final var totalAmount = getInt(line[11]);
-            final var otherAmount = totalAmount - evAmount;
-            evStats.add(new EVStats(line[0], date, evAmount, otherAmount, totalAmount));
+            csvReader.close();
+            return Optional.of(evStats);
+        } catch (IOException e) {
+            // Handle or log the exception accordingly
+            e.printStackTrace();
         }
-        csvReader.close();
-        return Optional.of(evStats);
+        return Optional.empty();
     }
 
     public record EVStats(String name, LocalDate date, int evAmount, int otherAmount, int totalAmount) {
