ace5cb3ad59f6e88cffb68491ee9e2493b8adcc2
start.vaadin.com <no-reply@vaadin.com>
2023-05-26 10:11:52 +0000

    Generated project

---
+++ src/main/java/com/vesanieminen/views/MainLayout.java
@@ -0,0 +1,80 @@
+package com.vesanieminen.views;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.vesanieminen.views.evstatistics.EVStatisticsView;
+import org.vaadin.lineawesome.LineAwesomeIcon;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H2 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addDrawerContent();
+        addHeaderContent();
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H2();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        addToNavbar(true, toggle, viewTitle);
+    }
+
+    private void addDrawerContent() {
+        H1 appName = new H1("EV stats");
+        appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        Header header = new Header(appName);
+
+        Scroller scroller = new Scroller(createNavigation());
+
+        addToDrawer(header, scroller, createFooter());
+    }
+
+    private SideNav createNavigation() {
+        // SideNav is a production-ready official component under a feature flag.
+        // However, it has accessibility issues and is missing some features.
+        // Both will be addressed in an upcoming minor version.
+        // These changes are likely to cause some breaking change to the custom css
+        // applied to the component.
+        SideNav nav = new SideNav();
+
+        nav.addItem(
+                new SideNavItem("EV Statistics", EVStatisticsView.class, LineAwesomeIcon.CHART_AREA_SOLID.create()));
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
