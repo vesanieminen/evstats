6c1506bda590e010c12a9159d7a15465e2516f96
Vesa Nieminen <vesa@vaadin.com>
2023-05-30 14:08:49 +0300

    Fixed the loadData method which retrieves the file online directly.

--- src/main/java/com/vesanieminen/services/AUT_FI_Service.java
+++ src/main/java/com/vesanieminen/services/AUT_FI_Service.java
@@ -6,17 +6,14 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.channels.Channels;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,27 +25,17 @@ import static com.vesanieminen.Utils.getMonth;
 
 public class AUT_FI_Service {
 
-    private final static String URL = "https://www.aut.fi/tilastot/ensirekisteroinnit/ensirekisteroinnit_kayttovoimittain/henkiloautojen_kayttovoimatilastot?download_8097=1";
+    private final static String URL = "https://www.aut.fi/tilastot/ensirekisteroinnit/ensirekisteroinnit_kayttovoimittain/henkiloautojen_kayttovoimatilastot?download_8097=1&download_8098=1";
     private final static String CSV_FILENAME = "aut.fi.csv";
     private final static String DEFAULT_CSV_FILENAME = "data/ensirekisteroinnit_kayttovoimat_jakauma-2.csv";
 
     // Doesn't work atm. due to the url serving a page instead of the actual file.
     public static HttpResponse<String> loadData() {
-        try {
-            final var readableByteChannel = Channels.newChannel(new URL(URL).openStream());
-            FileOutputStream fileOutputStream = new FileOutputStream(CSV_FILENAME);
-            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-            fileOutputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
         final HttpRequest request;
         final HttpResponse<String> response;
         try {
-            request = HttpRequest.newBuilder().uri(new URI(URL)).GET().build();
+            request = HttpRequest.newBuilder().version(HttpClient.Version.HTTP_2).uri(new URI(URL)).GET().build();
             response = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build().send(request, HttpResponse.BodyHandlers.ofString());
-            int i = 0;
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -62,11 +49,9 @@ public class AUT_FI_Service {
 
             CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
             CSVReader csvReader = new CSVReaderBuilder(reader)
-                    .withSkipLines(0)
+                    .withSkipLines(1)
                     .withCSVParser(parser)
                     .build();
-
-            String[] header = csvReader.readNext();
 
             String[] line;
             final List<EVStats> evStats = new ArrayList<>();
@@ -74,9 +59,9 @@ public class AUT_FI_Service {
             while ((line = csvReader.readNext()) != null) {
                 String month = line[0];
                 if (line[0].contains("/")) {
-                    final var split = line[0].split("/");
-                    year = split[0];
-                    month = split[1];
+                    final var yearAndMonth = line[0].split("/");
+                    year = yearAndMonth[0];
+                    month = yearAndMonth[1];
                 }
                 var date = LocalDate.of(Integer.parseInt(year), getMonth(month), 1);
                 date = date.withDayOfMonth(date.lengthOfMonth());
