2b23f59677c89ac1a4ef1a135fe83a728c045c7b
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-11-01 14:20:38 +0200

    Updated October 2024 data.

--- src/main/java/com/vesanieminen/views/statistics/TeslaRegistrationsBarView.java
+++ src/main/java/com/vesanieminen/views/statistics/TeslaRegistrationsBarView.java
@@ -42,7 +42,7 @@ public class TeslaRegistrationsBarView extends Main {
             chart.setHeightFull();
             final var configuration = chart.getConfiguration();
             final var teslaStats = evStats.get();
-            for (int month = 8; month >= 0; --month) {
+            for (int month = 9; month >= 0; --month) {
                 final var series = new ListSeries();
                 switch (month) {
                     case 0 -> series.setName("January");
@@ -54,7 +54,7 @@ public class TeslaRegistrationsBarView extends Main {
                     case 6 -> series.setName("July");
                     case 7 -> series.setName("August");
                     case 8 -> series.setName("September");
-                    //case 9 -> series.setName("October");
+                    case 9 -> series.setName("October");
                     //case 10 -> series.setName("November");
                     //case 11 -> series.setName("December");
                 }
